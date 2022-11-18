eac02b950bea0ebbf2cc7942bb8f299e68e19988
tkhito <joaovitorsoaresdosreis3@gmail.com>
2022-11-18 15:20:27 -0300

    Change the things

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
 (() => {
     var button = document.getElementsByTagName("button");
-    var screen = document.querySelector(".screen");
-    var cache = [];
-    var other = [];
+    var span = document.querySelector(".text");
     for(var i = 0;i < button.length;i++){
         button[i].addEventListener("click", (e) => {
-
-            if(e.target.innerText == "="){
-                screen.innerText = eval(screen.innerText);
+            if(e.target.innerText == "←"){
+              span.innerText = "";
+            }else if(e.target.innerText == "="){
+              try{
+                span.innerText = eval(span.innerText);
+              }catch(err){
+                span.style.fontSize = "20pt";
+                span.innerText = "The count is generate a error try again";
+                setTimeout(() => {
+                  span.innerText = "";
+                }, 5000)
+              }
+            }else if(span.innerText.length > 13){
+              span.style.fontSize = "20pt";
+              span.innerText = "Maximum number reached";
+              setTimeout(() => {
+                span.innerText = "";
+              }, 5000)
             }else {
-                screen.innerText += e.target.innerText;
+                span.innerText += e.target.innerText;
             }
+          span.style.fontSize = "30pt";
         });
     }
 
